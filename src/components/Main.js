1f939a02517f1943650f2eeca3e7c10a2ee65459
Jakob <me@jterhorst.de>
2021-07-21 23:31:52 +0200

    Get the name Input

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,19 @@ import Form from "./Form";
 import { useState } from "react";
 
 export default function Main() {
+  // Step 1 Getting the Name for Search Query
+  const [name, setName] = useState("");
+
+  function getName(event) {
+    event.preventDefault();
+    const nameFromInput = event.target.name.value;
+    setName(nameFromInput);
+    event.target.reset();
+  }
+
   return (
     <main>
-      <Form onSubmit={() => console.log("submit")} />
+      <Form onSubmit={getName} />
 
       <section className="result">
         <h2>Result</h2>
